c6ee49927ae56136d900005ade430f50b29648e8
Gompangii <gompangii@gmail.com>
2024-12-27 13:40:43 +0900

    10강 JDBC SELECT 테스트

--- src/main/java/com/sbs/java/jdbc/board/article/Article.java
+++ src/main/java/com/sbs/java/jdbc/board/article/Article.java
@@ -1,7 +1,11 @@
 package com.sbs.java.jdbc.board.article;
 
+import java.time.LocalDateTime;
+
 public class Article {
   private final int id;
+  private LocalDateTime regDate;
+  private LocalDateTime updateDate;
   private String subject;
   private String content;
 
@@ -11,22 +15,42 @@ public class Article {
     this.content = content;
   }
 
+  public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String subject, String content) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.subject = subject;
+    this.content = content;
+  }
+
   public int getId() {
     return id;
+  }
+
+  public LocalDateTime getRegDate() {
+    return regDate;
+  }
+
+  public LocalDateTime getUpdateDate() {
+    return updateDate;
   }
 
   public String getSubject() {
     return subject;
   }
-
   public String getContent() {
     return content;
   }
 
+  public void setRegDate(LocalDateTime regDate) {
+    this.regDate = regDate;
+  }
+  public void setUpdateDate(LocalDateTime updateDate) {
+    this.updateDate = updateDate;
+  }
   public void setSubject(String subject) {
     this.subject = subject;
   }
-
   public void setContent(String content) {
     this.content = content;
   }
@@ -35,6 +59,8 @@ public class Article {
   public String toString() {
     return "Article{" +
         "id=" + id +
+        ", regDate='" + regDate + '\'' +
+        ", updateDate='" + updateDate + '\'' +
         ", subject='" + subject + '\'' +
         ", content='" + content + '\'' +
         '}';
---
+++ src/test/java/JDBCSelectTest.java
@@ -0,0 +1,70 @@
+import com.sbs.java.jdbc.board.article.Article;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCSelectTest {  // 연결 테스트
+  //private  static  final String DB_URL = "jdbc:mysql://mariadb.mypc.myvbox:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull"; // 데이터베이스 URL
+  private  static  final String DB_URL = "jdbc:mysql://mariadb.mypc.myvbox:3306/text_board";
+  private  static  final String DB_USER = "root"; // MariaDB 사용자 이름
+  private  static  final String DB_PASSWORD = "mariadb"; // MariaDB 비밀번호
+
+  public static void main(String[] args) {
+    // MariaDB에 연결을 시도합니다.
+    Connection conn = null;
+    PreparedStatement pstat = null;
+    ResultSet rs = null;
+
+    List<Article> articles = new ArrayList<>();
+
+    try {
+      // JDBC 드라이버 로드
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+      System.out.println("데이터 베이스 연결 성공!");
+
+      String sql = "SELECT *" ;
+      sql += " FROM article";
+      sql += " ORDER BY id DESC;";
+
+      pstat = conn.prepareStatement(sql);
+
+      rs = pstat.executeQuery();
+
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        LocalDateTime regDate = rs.getTimestamp("regDate").toLocalDateTime();
+        LocalDateTime updateDate = rs.getTimestamp("updateDate").toLocalDateTime();
+        String subject = rs.getString("subject");
+        String content = rs.getString("content");
+
+        Article article = new Article(id, regDate, updateDate, subject, content);
+        articles.add(article);
+      }
+
+      System.out.println("결과 : " + articles);
+      
+    } catch (ClassNotFoundException e) {
+      System.err.println("JDBC 드라이버를 찾지 못했습니다.");
+      e.printStackTrace();
+    } catch (SQLException e) {
+      System.out.println("데이터 베이스 연결 실패");
+      e.printStackTrace();
+    } finally {
+      try {
+        if(rs != null && !rs.isClosed()) rs.close();
+        if(pstat != null && !pstat.isClosed()) pstat.close();
+        if(conn != null && !conn.isClosed()) {
+          conn.close();  // 데이터 베이스 연결 해제
+          System.out.println("데이터베이스 연결이 해제되었습니다.");
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
